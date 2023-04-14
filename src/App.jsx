c3c0f8e9e50bb68dcf02a383e5dc49808b6006da
Rolzi <aki.peltoniemi@gmail.com>
2023-04-14 20:03:35 +0300

    Map the dice array values to Dice components

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,31 @@ import React, { useState, useEffect } from "react";
 import Dice from "./components/Die.jsx";
 import "./App.css";
 
-function allNewDice() {
-  let dice = [];
-  for (let i = 0; i < 10; i++) {
-    dice.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return dice;
-}
-
-console.log(allNewDice());
-
 function App() {
+  function allNewDice() {
+    let dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return dice;
+  }
+
+  const [dice, setDice] = useState(allNewDice());
+
+  // useEffect(() => {
+  //   allNewDice();
+  // }, []);
+
+  let diceElements = dice.map((die, index) => {
+    return <Dice value={die} key={index} />;
+  });
+
   return (
     <div className="App">
       <main>
         <section className="gameArea">
           <h1>Tenzies</h1>
-          <div className="dice">
-            <Dice value={1} />
-            <Dice value={2} />
-            <Dice value={3} />
-            <Dice value={4} />
-            <Dice value={5} />
-            <Dice value={6} />
-            <Dice value={7} />
-            <Dice value={8} />
-            <Dice value={9} />
-            <Dice value={1} />
-          </div>
+          <div className="dice">{diceElements}</div>
         </section>
       </main>
     </div>
